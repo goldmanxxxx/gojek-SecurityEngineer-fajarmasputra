615581a53c08dcae8feebd36e25b57177be6189e
Goldman <putramasfajar@gmail.com>
2021-12-04 16:39:44 +0700

    Add Database Schemas
    First step, you should upload sql file into Database MySQL server. You can use xampp.

---
+++ noob.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2021 at 10:33 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `noob`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`id`, `message`) VALUES
+(1, 'Pembangunan Ekonomi Di Indonesia\r\nsalah satu masalah ekonomi di indonesia yang sering kita jumpai adalah pengangguran yang tiap tahun semakin meningkat.\r\n\r\nPenyebab Pengangguran\r\n\r\nPengangguran umumnya disebabkan karena jumlah angkatan kerja tidak sebanding dengan jumlahlapangan pekerjaan yang mampu menyerapnya. Pengangguran seringkali menjadi masalah dalamperekonomian karena dengan adanya pengangguran, produktivitas dan pendapatan masyarakat akan berkurang sehingga dapat menyebabkan timbulnya kemiskinan dan masalah-masalah sosial lainnya.\r\n\r\nTingkat pengangguran dapat dihitung dengan cara membandingkan jumlah pengangguran dengan jumlah angkatan kerja yang dinyatakan dalam persen.\r\n\r\nKetiadaan pendapatan menyebabkan penganggur harus mengurangi pengeluaran konsumsinya yang menyebabkan menurunnya tingkat kemakmuran dan kesejahteraan. Pengangguran yang berkepanjangan juga dapat menimbulkan efek psikologis yang buruk terhadap penganggur dankeluarganya.\r\n\r\nTingkat pengangguran yang terlalu tinggi juga dapat menyebabkan kekacauan politik, keamanan dan sosial sehingga mengganggu pertumbuhan dan pembangunan ekonomi. Akibat jangka panjang adalah menurunnya GNP dan pendapatan per kapita suatu negara.\r\n\r\nDi negara-negara berkembang seperti Indonesia, dikenal istilah “pengangguran terselubung” di mana pekerjaan yang semestinya bisa dilakukan dengan tenaga kerja sedikit, dilakukan oleh lebih banyak orang.\r\n\r\nPengangguran atau tuna karya adalah istilah untuk orang yang tidak bekerja sama sekali, sedang mencari kerja, bekerja kurang dari dua hari selama seminggu, atau seseorang yang sedang berusaha mendapatkan pekerjaan yang layak. Pengangguran umumnya disebabkan karena jumlah angkatan kerja atau para pencari kerja tidak sebanding dengan jumlah lapangan kerja yang ada yang mampu menyerapnya. Pengangguran seringkali menjadi masalah dalam perekonomian karena dengan adanya pengangguran, produktivitas dan pendapatan masyarakat akan berkurang sehingga dapat menyebabkan timbulnya kemiskinan dan masalah-masalah sosial lainnya.'),
+(2, 'Mau investasi Bitcoin dan aset Kripto seperti ETH, DOGE, BTC, USDT? Mending investasinya di Tokocrypto aja dan top up investasinya lebih mudah, cepat, aman pakai GoPay. Dengan Tokocrypto, kamu dapat melakukan investasi aset kripto dengan praktis dan mudah mulai dari minimum Rp50.000 lho.\r\n\r\nYuk top up buat investasi di Tokocrypto pakai GoPay, ada promo menarik juga yang nggak boleh kamu lewatkan. Simak selengkapnya di bawah ini ya!\r\n\r\nSyarat dan Ketentuan Promo Tokocrypto\r\nPeriode promo dari 1–31 Desember 2021 (00.00–23.59 WIB).\r\nPromo berupa cashback 2% (maksimum Rp30.000) dengan minimum pembelian Rp1.000.000.\r\nPromo berlaku 1x pengguna/cashback/akun/merchant/periode.\r\nBerlaku untuk Pengguna Baru di platform partner.\r\nKuota promo terbatas.\r\nDefinisi Pengguna Baru adalah pengguna yang belum pernah melakukan transaksi GoPay di platform partner.\r\nPromo hanya berlaku untuk pengguna yang melakukan pendaftaran di aplikasi Gojek sebelum tanggal 1 November 2021.\r\nCashback akan diterima setelah pengguna klik tombol \"Pakai\" atau “Apply promo” ketika berada di halaman checkout.\r\nApabila kamu sudah menghubungkan GoPay dengan aplikasi Tokocrypto, kamu akan mendapatkan cashback tanpa harus mengklik “Apply Promo” pada halaman checkout GoPay selama kuota promo masih tersedia.\r\nCashback akan diterima maksimal dalam 1x24 jam.\r\nBarang yang sudah dibeli menggunakan promo ini tidak dapat dikembalikan.\r\nCashback tidak akan berlaku bagi pengguna yang sudah melakukan refund selama periode promo berlangsung.\r\nPromo tidak dapat digabungkan dengan promo lainnya.\r\nPromo hanya berlaku untuk pembayaran menggunakan GoPay.\r\nGoPay berhak mengubah dan/atau menghentikan promo sewaktu-waktu tanpa pemberitahuan sebelumnya.\r\nPengguna dapat menyampaikan klaim pengembalian uang kepada kami selambat-lambatnya dalam waktu 7 (tujuh) hari kalender sejak tanggal transaksi dengan menyampaikan bukti pendukung yang cukup. Dalam hal demikian kami akan memproses pengembalian uang berdasarkan hasil tinjauan kami atas klaim tersebut. Dalam hal klaim pengembalian uang disetujui, GoPay berhak menarik kembali setiap nilai promo terkait. \r\nGoPay berhak menahan dan/atau membatalkan promo kepada pengguna dan/atau di rekan usaha tertentu sewaktu-waktu termasuk apabila transaksi yang mendasari promo dibatalkan dan/atau pengguna meminta refund atas transaksi yang mendasari promo, ditemukan tindakan penyalahgunaan (abusive), kecurangan (fraudulent), dan/atau aktivitas mencurigakan lainnya.\r\nCara Top Up Tokocrypto Pakai GoPay Melalui Desktop\r\nMasuk ke akun Tokocrypto kamu dan pilih Dompet. Lalu, klik Setoran.\r\nPilih setoran IDR, kemudian pilih tujuan dana yang ingin didepositkan.\r\nPilih GoPay dan masukkan jumlah setoran yang diinginkan.\r\nKlik Selanjutnya dan ikuti instruksi yang ada.\r\nBuka aplikasi Gojek dan pilih Bayar. Lalu, arahkan kamera ke QR Code.\r\nPastikan nominal deposit yang tertera sudah benar dan lakukan konfirmasi pembayaran sebelum 15 menit.\r\nSetelah deposit berhasil dilakukan, lakukan refresh website. Deposit akan segera masuk ke Dompet kamu dalam 30 menit atau tergantung trafik. \r\nRiwayat setoran kamu secara otomatis tertera pada kolom riwayat setoran.\r\nJika mengalami kendala, silakan hubungi Customer Support Tokocrypto melalui email: support@tokocrypto.com atau via Live Chat melalui website Tokocrypto'),
+(3, 'Tu passes au bar ?'),
+(4, 'Je ne pense vraiment pas que ca soit une bonne idée Moe... Homer doit aussi se soucier de sa famille, il n\'a toujours pas diné avec nous alors que c\'est le premier assis a table habituellement .... Je ne le comprend plus depuis qu\'il est devenu Administrateur de ce site \"Noob\" ... il ne fait que bosser .. Je ne reconnait plus mon homer :('),
+(5, 'Un brave type cet Homer ...');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(7) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `bio` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `lastname`, `password`, `image`, `bio`) VALUES
+(1, 'Homer', 'Simpson', 'Homer', 'img/pic.jpg', 'C\'est pas parce que je m\'en fous que je comprends pas.'),
+(2, 'Marge', 'Simpson', '70b959c40fe68e799d54296ba32834ef', 'img/pic2.jpg', 'Quand une femme dit que tout va bien, ça veut dire que tout va mal.'),
+(5, 'Montgomery', 'Burns', '9726255eec083aa56dc0449a21b33190', 'img/pic3.jpg', 'Famille, religion, amitié. Voici les 3 démons que vous devez abattre pour réussir dans les affaires.'),
+(6, 'Moe', 'Szyslak', '3e1867f5aee83045775fbe355e6a3ce1', 'img/pic4.jpg', 'On pourrait vendre de l\'alcool. Je fais ça très bien.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
